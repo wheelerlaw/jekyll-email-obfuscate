b564196421814cb944f419fc4ce3bbdb40210e00
Wheeler Law <whelderwheels613@gmail.com>
2016-11-28 23:55:09 -0500

    Enabled the usage of context variables

--- email.rb
+++ email.rb
@@ -4,13 +4,18 @@ module Jekyll
 
     MAIL_TO = '&#109;&#97;&#105;&#108;&#116;&#111;&#58;'
     STYLE = 'unicode-bidi: bidi-override; direction: rtl;'
-    PARAMETER_SYNTAX = %r!address="(?<address>[^"]+)" style="(?<style>[^"]+)"!
+    PARAMETER_SYNTAX = %r!address="(?<address_lit>[^"]+)"|address=(?<address_var>[^\S]+) style="(?<style>[^"]+)"!
 
     def initialize(tag_name, text, tokens)
       super
       matched = text.strip.match(PARAMETER_SYNTAX)
       if matched
-        @email = matched['address'].strip
+        if matched['address_var']
+          @email_var = matched['address_var'].strip
+        elsif matched['address_lit']
+          @email = matched['address_lit'].strip
+        end
+
         @style = matched['style'].strip
       else
         @email, @style = text.strip.split(%r!\s+!, 2)
@@ -20,6 +25,9 @@ module Jekyll
     def render(context)
       # We can't encode just once. Reversal of html entities will
       # not work as expected.
+      if @email_var
+        @email = resolve(context, @email_var)
+      end
       reversed = encode(@email.each_char.to_a.reverse.join)
       obfuscated = encode(@email)
       if @style == "link"
@@ -40,6 +48,15 @@ module Jekyll
     def encode(str)
       str = str.gsub('@', '&#64;')
       str = str.gsub('.', '&#46;')
+    end
+
+    def resolve(context, var_name)
+      matched = /^(?<curr>[^.]+)\.(?<next>.+)/.match(var_name)
+      if matched
+        resolve(context[matched['curr']], matched['next'])
+      else
+        context[var_name]
+      end
     end
   end
 end
